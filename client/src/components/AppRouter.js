5f18ca28cf08a90e2bf35b1d23ba610818a37940
VipDipp <kuxaruk03@ukr.net>
2022-02-10 22:18:20 +0200

    main client logic

--- client/src/components/AppRouter.js
+++ client/src/components/AppRouter.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import { Routes, Route, Redirect } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { Context } from '..';
 import { authRoutes, publicRoutes } from '../routes';
 
 function AppRouter() {
-    const isAuth = false;
+    const {user} = useContext(Context);
+    console.log(user);
+
     return ( 
         <Routes>
-            {isAuth && authRoutes.map(({path, Component}) =>
+            {user.isAuth && authRoutes.map(({path, Component}) =>
                 <Route key={path} path={path} element={<Component />} exact/>
             )}
             {publicRoutes.map(({path, Component}) =>
                 <Route key={path} path={path} element={<Component />} exact/>
             )}
+            <Route path="*" element={<Navigate to="/" />}/>
         </Routes>
     );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import UserStore from './store/UserStore';
 
-ReactDOM.render( <
-    App / > ,
+ export const Context = createContext(null);
+
+ReactDOM.render( 
+    <Context.Provider value={{
+        user: new UserStore()
+    }}>
+        <App /> 
+    </Context.Provider>,
     document.getElementById('root')
+    
 );
