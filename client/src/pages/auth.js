efe26f9ed1040883c4dc7bb1e58cd22bd722762e
VipDipp <kuxaruk03@ukr.net>
2022-02-13 22:39:08 +0200

    update

--- client/src/pages/auth.js
+++ client/src/pages/auth.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import { Button, Container, Form, Row } from 'react-bootstrap';
 import Card from 'react-bootstrap/Card';
-import { NavLink } from 'react-router-dom';
-import { REGISTRATION_ROUTE } from '../utils/consts';
+import { NavLink, useLocation } from 'react-router-dom';
+import { login } from '../../../server/controllers/userController';
+import { registration } from '../http/userAPI';
+import { LOGIN_ROUTE, REGISTRATION_ROUTE } from '../utils/consts';
 
 function Auth() {
+    const location = useLocation();
+    const isLogin = location.pathname === LOGIN_ROUTE;
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const click = async () => {
+        if (isLogin) {
+            const response = await login();
+        } else {
+            const response = await registration();
+            console.log(response);
+        }
+    }
+
     return ( 
         <div>
             <Container 
@@ -12,25 +28,35 @@ function Auth() {
                 style={{height: window.innerHeight - 54}}
             >
                 <Card style={{width: 600}} className='p-5'>
-                    <h2 className='m-auto'>Авторизация</h2>
+                    <h2 className='m-auto'> {isLogin ? 'Авторизация' : 'Регистрация'}</h2>
                     <Form className='d-flex flex-column'>
                         <Form.Control 
                             className='mt-3'
                             placeholder='Введите ваш e-mail...' 
+                            value={email}
+                            onChange={e => setEmail()}
                         />
                         <Form.Control 
                             className='mt-3'
                             placeholder='Введите ваш пароль...' 
+                            value={password}
+                            onChange={e => setPassword()}
                         />
                         <Row className='d-flex justify-content-between mt-3 pl-3 pr-3'>
+                            {isLogin ?
                             <div>
                                 Нет аккаунта? <NavLink to={REGISTRATION_ROUTE}>Зарегистрируйся!</NavLink>
                             </div>
+                            :
+                            <div>
+                                Есть аккаунт? <NavLink to={LOGIN_ROUTE}>Войдите!</NavLink>
+                            </div>
+                            }
                             <Button  
                                 className='align-self-end'
                                 variant={'outline-success'}
                             >
-                                Войти
+                                {isLogin ? 'Войти' : 'Зарегестрироваться'} 
                             </Button>
                         </Row>
                     </Form>
